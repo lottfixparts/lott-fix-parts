2aa9c9b11075f080e0494e57e5f86dc7d6a7e5ab
lottfixparts <lucasrongo@gmail.com>
2025-10-19 23:35:57 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx,jsx,js}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}

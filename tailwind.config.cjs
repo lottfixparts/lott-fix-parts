b884332603b840543805b0a52311f32905d13f59
lottfixparts <lucasrongo@gmail.com>
2025-10-20 22:29:58 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1F2937",
+      },
+    },
+  },
   plugins: [],
 };

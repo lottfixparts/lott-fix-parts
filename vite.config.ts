196d1d5b6cead345f18f85e4bed1009238347296
lottfixparts <lucasrongo@gmail.com>
2025-10-26 23:11:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 });

c78a9af8792418a0bb0ff3352366734f19924ceb
lottfixparts <lucasrongo@gmail.com>
2025-10-27 21:45:33 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,6 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // CONFIGURACIÓN GLOBAL – LOTT FIX & PARTS
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-// URL del WebApp de Google Apps Script (editar más adelante)
-export const GAS_WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbzjnuaRgHZNjwIdyJEJ1sPDIWIPXRKvhQdKCguQjbqxhXpafUGu2_1mXnhlP27470j0/exec";
 // Logo por defecto (Base64 generado desde tu imagen Standard.jpg)
 export const DEFAULT_LOGO =
   "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/4gHYSUNDX1BST0ZJTEUAAQEAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAA";

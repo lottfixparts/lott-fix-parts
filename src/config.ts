7469d5b5fdc39ca8b0f362f1fc4a646b3a4d647a
lottfixparts <lucasrongo@gmail.com>
2025-10-29 22:07:19 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // URL del WebApp (Backend GAS)
 export const GAS_WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbw5dKU78nClkGVLNrmcNxPWAF740te170QRuU2_bqtveIp2fXBhIhbz8jpuLnTlQ1M/exec";
+  "https://script.google.com/macros/s/AKfycbz8feAdIKVRwSeISV7tquUdbUWa6Mhh_9DhiReDVc-N0MVa_xHVh3Yk5QBRLCmz_Go/exec";

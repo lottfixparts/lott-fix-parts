12cccb0756f46b1adb25c0bfdeed9ceabd5b51f1
lottfixparts <lucasrongo@gmail.com>
2025-10-31 02:22:24 +0000

    fix: correct jspdf dependency

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // URL del WebApp (Backend GAS)
 export const GAS_WEBAPP_URL =
-  "https://script.google.com/macros/s/AKfycbz_yB9d-qYDaCfOLC-zpiyZTG5ITzrYnKqbsP7_8rRYU4uSHAsWDoD57CKGalVSJ405/exec";
+  "https://script.google.com/macros/s/AKfycbzWphiWTEdVNmtGUYWyhH-E-WByyuEN6OhcLBp76ujDaoI3fjIvWqhZMZGqt5mgtDJK/exec";
